5736b5b068dc7706f8ba1f3968efacee15967869
chdhatri <dhatric@sarma-2.local>
2018-08-20 11:04:02 +0530

    Merge branch 'master' of https://github.com/chdhatri/DriveSquared-Carnd into DriveSquared
    # Conflicts:
    #	ros/src/waypoint_updater/waypoint_updater.py

--- ros/src/waypoint_updater/waypoint_updater.py
+++ ros/src/waypoint_updater/waypoint_updater.py
@@ -83,10 +83,9 @@ class WaypointUpdater(object):
 
     def waypoints_cb(self, waypoints):
         self.base_waypoints = waypoints
-            if not self.waypoints_2d:
-                self.waypoints_2d = [[waypoint.pose.pose.position.x, waypoint.pose.pose.position.y] for waypoint in waypoints.waypoints ]
-                self.waypoint_tree = KDTree(self.waypoints_2d)
-    pass
+        if not self.waypoints_2d:
+            self.waypoints_2d = [[waypoint.pose.pose.position.x, waypoint.pose.pose.position.y] for waypoint in waypoints.waypoints ]
+            self.waypoint_tree = KDTree(self.waypoints_2d)
 
     def traffic_cb(self, msg):
     # TODO: Callback for /traffic_waypoint message. Implement
