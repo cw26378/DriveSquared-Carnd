1dd252cd6f5d0343eb60d8b6bc5917678fc99812
cw26378 <wangcheng@utexas.edu>
2018-08-20 22:07:08 -0700

    waypoint_partial and DBW node

--- ros/src/twist_controller/twist_controller.py
+++ ros/src/twist_controller/twist_controller.py
@@ -1,14 +1,85 @@
 
+import rospy
+import math
+
+from yaw_controller import YawController
+from pid import PID
+from lowpass import LowPassFilter
+
+
 GAS_DENSITY = 2.858
 ONE_MPH = 0.44704
-
+MIN_SPEED = 0.1
 
 class Controller(object):
-    def __init__(self, *args, **kwargs):
+    def __init__(self, vehicle_mass, fuel_capacity, brake_deadband,
+                 decel_limit, accel_limit,
+                 wheel_radius, wheel_base, steer_ratio,
+                 max_lat_accel, max_steer_angle):
         # TODO: Implement
-        pass
-
-    def control(self, *args, **kwargs):
+        ## yaw controller
+        self.yaw_controller = YawController(wheel_base, steer_ratio, MIN_SPEED, max_lat_accel,                                              max_steer_angle)
+        
+        ## pid controller for velocity throttle control
+        kp = 0.3
+        ki = 0.1
+        kd = 0.0
+        min_throttle = 0
+        max_throttle = 0.2
+        self.throttle_controller = PID(kp, ki, kd, min_throttle, max_throttle)
+        
+        ## low pass filter
+        tau = 0.5 # 1/(2Pi*tau) = cut-off frequency
+        ts = 0.02 # sample time
+        self.vel_lpf = LowPassFilter(tau,ts)
+        
+        self.vehicle_mass = vehicle_mass
+        self.fuel_capacity = fuel_capacity
+        self.brake_deadband = brake_deadband
+        self.decel_limit = decel_limit
+        self.accel_limit = accel_limit
+        self.wheel_radius = wheel_radius
+        self.steer_ratio = steer_ratio
+        
+        self.last_time = rospy.get_time()
+        self.last_vel = None
+    
+    def control(self, current_vel, dbw_enabled, linear_vel, angular_vel):
         # TODO: Change the arg, kwarg list to suit your needs
         # Return throttle, brake, steer
-        return 1., 0., 0.
+        
+        if not dbw_enabled:
+            # reset the integral value of PID controller to zero (dealing with incorrect accumulation)
+            self.throttle_controller.reset()
+            # stay put when not dbw_enabled
+            return 0., 0., 0.
+        
+        current_vel = self.vel_lpf.filt(current_vel)
+        
+        steering = self.yaw_controller.get_steering(linear_vel, angular_vel, current_vel)
+        #steering = angular_vel * self.steer_ratio
+        
+        rospy.loginfo("calculated steering angle from yaw_controller: %.2f", steering)
+        vel_error = linear_vel - current_vel
+        self.last_vel = current_vel
+        
+        current_time = rospy.get_time()
+        sample_time = current_time - self.last_time
+        self.last_time = current_time
+        
+        throttle = self.throttle_controller.step(vel_error, sample_time)
+        brake = 0
+        
+        if linear_vel == 0 and current_vel < MIN_SPEED:
+            brake = 700 # torque N*m
+            throttle = 0
+        
+        elif throttle < 0.1 and vel_error < 0:
+            throttle = 0
+            decel = max(vel_error, self.decel_limit)
+            brake = abs(decel) * self.vehicle_mass * self.wheel_radius
+            
+    return throttle, brake, steering
+
+#return 1., 0., 0.
+
--- ros/src/waypoint_updater/waypoint_updater.py
+++ ros/src/waypoint_updater/waypoint_updater.py
@@ -1,67 +1,112 @@
 #!/usr/bin/env python
 
 import rospy
+import numpy as np
 from geometry_msgs.msg import PoseStamped
 from styx_msgs.msg import Lane, Waypoint
-
+from scipy.spatial import KDTree
 import math
 
 '''
-This node will publish waypoints from the car's current position to some `x` distance ahead.
+    This node will publish waypoints from the car's current position to some `x` distance ahead.
+    
+    As mentioned in the doc, you should ideally first implement a version which does not care
+    about traffic lights or obstacles.
+    
+    Once you have created dbw_node, you will update this node to use the status of traffic lights too.
+    
+    Please note that our simulator also provides the exact location of traffic lights and their
+    current status in `/vehicle/traffic_lights` message. You can use this message to build this node
+    as well as to verify your TL classifier.
+    
+    TODO (for Yousuf and Aaron): Stopline location for each traffic light.
+    '''
 
-As mentioned in the doc, you should ideally first implement a version which does not care
-about traffic lights or obstacles.
-
-Once you have created dbw_node, you will update this node to use the status of traffic lights too.
-
-Please note that our simulator also provides the exact location of traffic lights and their
-current status in `/vehicle/traffic_lights` message. You can use this message to build this node
-as well as to verify your TL classifier.
-
-TODO (for Yousuf and Aaron): Stopline location for each traffic light.
-'''
-
-LOOKAHEAD_WPS = 200 # Number of waypoints we will publish. You can change this number
+LOOKAHEAD_WPS = 100 # Number of waypoints we will publish. You can change this number
 
 
 class WaypointUpdater(object):
     def __init__(self):
         rospy.init_node('waypoint_updater')
-
+        
         rospy.Subscriber('/current_pose', PoseStamped, self.pose_cb)
         rospy.Subscriber('/base_waypoints', Lane, self.waypoints_cb)
-
+        
         # TODO: Add a subscriber for /traffic_waypoint and /obstacle_waypoint below
-
-
+        
+        
         self.final_waypoints_pub = rospy.Publisher('final_waypoints', Lane, queue_size=1)
-
+        
         # TODO: Add other member variables you need below
-
-        rospy.spin()
-
+        self.pose = None
+        self.base_waypoints = None
+        self.waypoints_2d = None
+        self.waypoint_tree = None
+        self.loop()
+    #rospy.spin()
+    
+    def loop(self):
+        rate = rospy.Rate(50)
+        while not rospy.is_shutdown():
+            if self.pose and self.base_waypoints:
+                #get closest waypoint
+                closest_waypoint_idx = self.get_closest_waypoint_idx()
+                self.publish_waypoints(closest_waypoint_idx)
+            rate.sleep()
+    
+    def get_closest_waypoint_idx(self):
+        x = self.pose.pose.position.x
+        y = self.pose.pose.position.y
+        closest_idx = self.waypoint_tree.query([x, y], 1)[1]
+        
+        #check the waypoint's relative position with respect to the vehicle
+        closest_coord = self.waypoints_2d[closest_idx]
+        prev_coord = self.waypoints_2d[closest_idx - 1]
+        
+        closest_vect = np.array(closest_coord)
+        prev_vect = np.array(prev_coord)
+        pos_vect = np.array([x, y])
+        
+        dir_dot = np.dot(closest_vect - prev_vect, pos_vect - closest_vect)
+        
+        if dir_dot > 0:
+            closest_idx = (closest_idx + 1) % len(self.waypoints_2d)
+        return closest_idx
+    
+    def publish_waypoints(self, closest_idx):
+        lane = Lane()
+        lane.header = self.base_waypoints.header
+        lane.waypoints = self.base_waypoints.waypoints[closest_idx: closest_idx + LOOKAHEAD_WPS]
+        self.final_waypoints_pub.publish(lane)
+    
     def pose_cb(self, msg):
         # TODO: Implement
-        pass
-
+        self.pose = msg
+    # pass
+    
     def waypoints_cb(self, waypoints):
         # TODO: Implement
-        pass
+        self.base_waypoints = waypoints
+        if not self.waypoints_2d:
+            self.waypoints_2d = [[waypoint.pose.pose.position.x, waypoint.pose.pose.position.y]
+                                 for waypoint in waypoints.waypoints]
+            self.waypoint_tree = KDTree(self.waypoints_2d)
+    # pass
 
-    def traffic_cb(self, msg):
-        # TODO: Callback for /traffic_waypoint message. Implement
-        pass
-
+def traffic_cb(self, msg):
+    # TODO: Callback for /traffic_waypoint message. Implement
+    pass
+    
     def obstacle_cb(self, msg):
         # TODO: Callback for /obstacle_waypoint message. We will implement it later
         pass
-
+    
     def get_waypoint_velocity(self, waypoint):
         return waypoint.twist.twist.linear.x
-
+    
     def set_waypoint_velocity(self, waypoints, waypoint, velocity):
         waypoints[waypoint].twist.twist.linear.x = velocity
-
+    
     def distance(self, waypoints, wp1, wp2):
         dist = 0
         dl = lambda a, b: math.sqrt((a.x-b.x)**2 + (a.y-b.y)**2  + (a.z-b.z)**2)
